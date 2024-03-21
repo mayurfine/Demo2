de23a0bcb685e2d074e57cf8b1f37db013ec946a
mayurfine <mayur.pondkule@fineshift.com>
2024-03-21 11:56:32 +0530

    First Commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> l=new ArrayList<>();
+        l.add(2);
+        l.add(3);
+        l.add(4);
+        System.out.println(l);
+
+
+    }
+
+}
